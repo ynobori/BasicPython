f3f086f0bb3f8e3925691ecde9d7e51e284e3256
ynobori <y.nobori.24.4.1354@4nonome.com>
2024-05-01 15:08:39 +0900

    制御構文_問2台系積分

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,17 @@
 from math import sin
+import math
 # --example--
 # print(sin(0))
 # >>> 0
-# -----------
+# ----------
+
+n = 100
+a = 0
+b = math.pi / 2
+h = (b - a) / n
+
+s = 0
+k = 1
+for k in range (101):
+    s += h * (sin(a + (k-1) * h) + sin(a + k*h)) / 2
+print(s)
