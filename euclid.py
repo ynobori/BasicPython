c5f15cd16cf26329521c3815a50e1346ff6f1bd9
ynobori <y.nobori.24.4.1354@4nonome.com>
2024-05-01 15:58:04 +0900

    制御構文_問3ユークリッドの互助法

--- euclid.py
+++ euclid.py
@@ -1,4 +1,20 @@
 a = input("a の値を入力: ")
 b = input("b の値を入力: ")
+a = int(a)
+b = int(b)
 
 # TODO
+"""
+if a < b:
+    c = a
+    a = b
+    b = c
+"""
+while True:
+    d = a % b
+    if d == 0:
+        print('最大公約数は',b)
+        break
+    else:    
+        a = b
+        b = d
