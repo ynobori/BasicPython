15c68714f5d77bad4efacafed3c59ffc2908ecb7
ynobori <y.nobori.24.4.1354@4nonome.com>
2024-05-01 14:44:41 +0900

    制御構文_問1素数判定

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,28 @@
 a = input("aの値を入力: ")
 b = input("bの値を入力: ")
+a = int(a)
+b = int(b)
 
 # TODO
+i = 2
+while a > i:
+    if a % i == 0:
+        print('aは素数ではありません')
+        break
+    elif i > a/2:
+        print('aは素数です')
+        break
+    else:
+        i += 1
+
+
+j = 2
+while b > j:
+    if b % j == 0:
+        print('bは素数ではありません')
+        break
+    elif j > b/2:
+        print('bは素数です')
+        break
+    else:
+        j += 1
