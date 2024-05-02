1f1676522b45942e73a787a29c0035e41f1f5d6a
ynobori <y.nobori.24.4.1354@4nonome.com>
2024-05-02 14:52:42 +0900

    関数_問3_ユークリッドの互除法

--- euclid.py
+++ euclid.py
@@ -1,4 +1,18 @@
 a = input("a の値を入力: ")
 b = input("b の値を入力: ")
+a = int(a)
+b = int(b)
 
 # TODO
+def euclid(a, b):
+    while True:
+        d = a % b
+        if d == 0:
+            return b
+            break
+        else:    
+            a = b
+            b = d
+
+gcd = euclid(a, b)
+print('最大公約数は',gcd)
