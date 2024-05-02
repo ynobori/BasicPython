572382fcd3054d85008e37139697b78525ccd34f
ynobori <y.nobori.24.4.1354@4nonome.com>
2024-05-02 13:18:36 +0900

    関数_問1_素数判定

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,25 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+n = input("nの値を入力: ")
+n = float(n)
+
 
 # TODO
+
+def prime(n):
+    if int(n) != n:
+        return False
+    elif n <= 1:
+        return False
+    else:
+        i = 2
+        while n > i:
+            if n % i == 0:
+                return False
+                break
+            elif i > n/2:
+                return True
+                break
+            else:
+                i += 1
+
+
+print(prime(n))
