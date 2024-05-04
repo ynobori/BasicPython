6129f5ce3a618a41b4f23bcb04c2ee02932ca37a
ynobori <y.nobori.24.4.1354@4nonome.com>
2024-05-04 13:52:50 +0900

    制御構文_修正_1

--- prime_number.py
+++ prime_number.py
@@ -4,25 +4,31 @@ a = int(a)
 b = int(b)
 
 # TODO
-i = 2
-while a > i:
-    if a % i == 0:
-        print('aは素数ではありません')
-        break
-    elif i > a/2:
-        print('aは素数です')
-        break
-    else:
-        i += 1
+if a == 1:
+    print(a,'は素数ではありません')
+else:
+    i = 2
+    while a > i:
 
+        if a % i == 0:
+            print(a,'は素数ではありません')
+            break
+        elif i > a/2:
+            print(a,'は素数です')
+            break
+        else:
+            i += 1
 
-j = 2
-while b > j:
-    if b % j == 0:
-        print('bは素数ではありません')
-        break
-    elif j > b/2:
-        print('bは素数です')
-        break
-    else:
-        j += 1
+if b == 1:
+    print(b,'は素数ではありません')
+else:
+    j = 2
+    while b > j:
+        if b % j == 0:
+            print(b,'は素数ではありません')
+            break
+        elif j > b/2:
+            print(b,'は素数です')
+            break
+        else:
+            j += 1
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -11,7 +11,6 @@ b = math.pi / 2
 h = (b - a) / n
 
 s = 0
-k = 1
-for k in range (101):
+for k in range (1, n + 1):
     s += h * (sin(a + (k-1) * h) + sin(a + k*h)) / 2
 print(s)
